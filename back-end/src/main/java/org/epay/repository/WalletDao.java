d40b08dc8e8686eb22bd7cadda6d78fbd40b4d4c
planetofthecode <163581636+planetofthecode@users.noreply.github.com>
2024-03-20 16:32:14 +0530

    Delete duplicate back-end/src/main directory

--- back-end/src/main/java/org/epay/repository/WalletDao.java
+++
@@ -1,8 +0,0 @@
-package org.epay.repository;
-
-import org.epay.model.Wallet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WalletDao extends JpaRepository<Wallet, String> {
-
-}
